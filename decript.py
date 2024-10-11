70735d8ddf435e5a75f1bd9d6ffb7b82db121978
FRANCISCO LEONARDO DE SOUZA JUNIOR <33257894+leosouzajr@users.noreply.github.com>
2024-10-11 13:01:44 -0300

    Update decript.py

--- decript.py
+++ decript.py
@@ -1,9 +1,9 @@
-def ksa(key):
-    key_length = len(key)
+def ksa(chave):
+    comprimento_chave = len(chave)
     S = list(range(256))
     j = 0
     for i in range(256):
-        j = (j + S[i] + key[i % key_length]) % 256
+        j = (j + S[i] + chave[i % comprimento_chave]) % 256
         S[i], S[j] = S[j], S[i]
     return S
 
@@ -16,22 +16,29 @@ def prga(S):
         S[i], S[j] = S[j], S[i]
         yield S[(S[i] + S[j]) % 256]
 
-def rc4_encrypt(text, key):
-    key = [ord(c) for c in key]
-    S = ksa(key)
-    keystream = prga(S)
-    encrypted_text = [format((ord(char) ^ next(keystream)), '02x') for char in text]
-    return ''.join(encrypted_text)
+def rc4_descriptografar(texto_criptografado, chave):
+    chave = [ord(c) for c in chave]
+    S = ksa(chave)
+    fluxo_chave = prga(S)
+    bytes_criptografados = [int(texto_criptografado[i:i+2], 16) for i in range(0, len(texto_criptografado), 2)]
+    texto_descriptografado = ''.join([chr(byte ^ next(fluxo_chave)) for byte in bytes_criptografados])
+    
+    return texto_descriptografado
 
-# Recebe a mensagem e a chave do usuário
-mensagem = input("Digite a mensagem que deseja criptografar: ")
+mensagem_criptografada = input("Digite a mensagem criptografada (em hexadecimal): ")
 chave = input("Digite a chave de criptografia: ")
+mensagem_descriptografada = rc4_descriptografar(mensagem_criptografada, chave)
+print("Mensagem descriptografada: ", mensagem_descriptografada)
 
-# Criptografa a mensagem usando o RC4
-mensagem_criptografada = rc4_encrypt(mensagem, chave)
 
-# Imprime a mensagem criptografada
-print("Mensagem criptografada: ", mensagem_criptografada)
+
+
+
+
+
+
+
+
 
 
 
